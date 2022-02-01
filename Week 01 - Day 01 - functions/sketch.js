479058e46d9591da01e8f19238a88b91974f385a
OleksandrMustakaiev <N00202022@iadt.ie>
2022-02-01 16:21:40 +0000

    added box function

--- Week 01 - Day 01 - functions/sketch.js
+++ Week 01 - Day 01 - functions/sketch.js
@@ -1,5 +1,29 @@
 //let distance = 23;
+//-------------------
+let boxWidth = 10;
+let boxHeight = 500;
+let spacing = 5;
+let numBoxes = 20;
+let xOffset = 0;
+let yOffset = 0;
 
+function setup(){
+    createCanvas(500,500);
+    background(0);
+}
+
+function draw(){
+    drawBoxes();
+}
+
+function drawBoxes(){
+    fill(255,0,0);
+    noStroke();
+    for(let i=0; i<numBoxes; i++){
+        let totalSpace = boxWidth+spacing;
+        rect(i*totalSpace + xOffset,yOffset,boxWidth,boxHeight)
+    }
+}
 //-------------------
 clap(3, 8); //we can have multiple parameters 
 
@@ -8,5 +32,12 @@ function clap(startNum, endNum){
     for(let i=0;i<loopCount;i++){
         console.log("Hi " + (startNum+i))
     }
+    return "Done";
+}
+//-------------------
+function addMeUp(num01, num02){
+    let total = num01 + num02; //expressions
+    console.log("all my work is done")
+    return total;
 }
 //-------------------
