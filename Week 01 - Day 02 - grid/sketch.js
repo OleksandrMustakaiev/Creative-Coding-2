acd06a28ffd633ca81c39840c4eed0bd38bb49cc
OleksandrMustakaiev <N00202022@iadt.ie>
2022-02-08 16:12:14 +0000

    make some changes

--- Week 01 - Day 02 - grid/sketch.js
+++ Week 01 - Day 02 - grid/sketch.js
@@ -1,12 +1,13 @@
 //-------------------
 function setup() {
     createCanvas(500,500);
-    background(125);
+    background(255);
+    angleMode(DEGREES);
+    rectMode(CENTER);
 }
 
 function draw() {
-    drawGrid(50, color("#accff44"), 1);
+    drawGrid(50, color("#ccff44"), 1);
     drawGrid(10, color("black"), 2);
 }
-
 //-------------------s
--- Week 01 - Day 02 - grid/utilities.js
+++ Week 01 - Day 02 - grid/utilities.js
@@ -1,11 +1,14 @@
 function drawGrid(numBoxes, strokeColor, strokeThickness){
     let boxSize = width / numBoxes;
-    noFill();
-    strokeWeight(strokeThickness);
-    stroke(strokeColor);
     for(j=0; j<numBoxes; j++){
         for(i=0; i<numBoxes; i++){
-        rect(boxSize*i, boxSize*j, boxSize, boxSize);
+            noFill();
+            strokeWeight(strokeThickness);
+            stroke(strokeColor);
+            push();
+            translate(i*boxSize, j*boxSize);
+            rect(0, 0, boxSize, boxSize);
+            pop();
         }
     }
 }
