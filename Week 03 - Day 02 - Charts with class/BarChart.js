032212a8de72b39bcf78dcc3be8d1e7954fefb6c
OleksandrMustakaiev <N00202022@iadt.ie>
2022-03-08 17:32:41 +0000

    make some changes on vertical chart

--- Week 03 - Day 02 - Charts with class/BarChart.js
+++ Week 03 - Day 02 - Charts with class/BarChart.js
@@ -2,25 +2,29 @@ class BarChart{
     constructor(_data){
         this.data = _data;
 
-        this.chartHeight = 200;
-        this.chartWidth = 200;
-        this.spacing = 10;
-        this.margin = 20;
-        this.numTicks = 10;
-        this.posX = 100;
-        this.posY = 400;
+        //set
+        this.chartHeight = 200; //chart height
+        this.chartWidth = 200; //chart width
+        this.posX = 100; //position x value
+        this.posY = 400; //position y value
+        this.spacing = 10; //space between each bars
+        this.margin = 20; //space bars between chart
+        this.numTicks = 7; //number of ticks and numbers(text)
+        this.numPlaces = 0; //number of decimal numbers of ticks
+        this.title = "Vertical Bar Chart"; //name of the title
+        this.fontSize = 12;
+        
         this.tickIncrements;
         this.maxValue;
-        this.numPlaces = 1;
         this.tickSpacing;
         this.barWidth;
-        this.availableWidth;
+        this.availableHeight;
 
-        this.showValues = true;
+        this.showValues = false;
         this.showLabels = true;
         this.rotateLabels = true;
 
-        this.colors = [color('#ffe066'), color('#fab666'), color('#f68f6a'), color('#f3646a')];
+        this.colors = [color('#6a89cc'), color('#82ccdd'), color('#b8e994'), color('#38ada9')];
 
         this.updateValues();
         this.calculateMaxValue();
@@ -47,6 +51,8 @@ class BarChart{
     this.drawTicks();
     this.drawHorizontalTicks();
     this.drawRects();
+    this.drawTitle();
+    this.drawLines();
     pop()
     }
 
@@ -56,7 +62,7 @@ class BarChart{
     }
     
     drawAxis(){
-        stroke(255, 180);
+        stroke('#747d8c');
         strokeWeight(2);
         line(0, 0, 0, -this.chartHeight); //y
         line(0, 0, this.chartWidth, 0); //x
@@ -65,14 +71,14 @@ class BarChart{
     drawTicks(){
         for(let i=0; i<=this.numTicks; i++){
             //ticks
-            stroke(255);
+            stroke('#747d8c');
             strokeWeight(1);
             line(0, this.tickSpacing * -i, -10, this.tickSpacing * -i);
     
             //numbers (text)
-            fill(255, 200);
+            fill('#747d8c');
             noStroke();
-            textSize(11);
+            textSize(this.fontSize);
             textAlign(RIGHT, CENTER);
             text((i * this.tickIncrements).toFixed(this.numPlaces), -15, this.tickSpacing * -i);
         }
@@ -81,7 +87,7 @@ class BarChart{
     drawHorizontalTicks(){
             for(let i=0; i<=this.numTicks; i++){
                 //horizontal lines
-                stroke(255);
+                stroke('#a4b0be');
                 strokeWeight(0.2);
                 line(0, this.tickSpacing * -i, this.chartWidth, this.tickSpacing * -i);
             }
@@ -96,12 +102,12 @@ class BarChart{
             fill(this.colors[colorNumber]);
             noStroke();
             rect((this.barWidth + this.spacing) * i, 0, this.barWidth, this.scaledData(-this.data[i].total));
-    
+            
             //numbers (text)
             if(this.showValues){
                 noStroke();
-                fill(255);
-                textSize(16);
+                fill('#747d8c');
+                textSize(this.fontSize);
                 textAlign(CENTER, BOTTOM);
                 text(this.data[i].total, ((this.barWidth + this.spacing) * i) + this.barWidth / 2, this.scaledData(-this.data[i].total));
             }
@@ -111,8 +117,8 @@ class BarChart{
                 if(this.rotateLabels){
                     push();
                     noStroke();
-                    fill(255);
-                    textSize(14);
+                    fill('#747d8c');
+                    textSize(this.fontSize);
                     textAlign(LEFT, CENTER);
     
                     translate(((this.barWidth + this.spacing) * i) + this.barWidth / 2, 10);
@@ -121,8 +127,8 @@ class BarChart{
                     pop();
                 }    else{
                     noStroke();
-                    fill(255);
-                    textSize(14);
+                    fill('#747d8c');
+                    textSize(this.fontSize);
                     textAlign(CENTER, BOTTOM);
                     text(this.data[i].name, ((this.barWidth + this.spacing) * i) + this.barWidth / 2, 20);
                 }
@@ -130,5 +136,33 @@ class BarChart{
             }
         }
         pop();
+    }
+
+    drawLines(){
+        push();
+        beginShape();
+        translate(this.margin + (this.barWidth / 2), 0);
+            for(let i=0; i<this.data.length; i++){
+                noFill();
+                stroke('#a4b0be');
+                strokeWeight(3);
+                vertex(i * 53, -this.scaledData(this.data[i].median))
+            }
+        endShape();
+        
+        for(let i=0; i<this.data.length; i++){
+            fill('#a4b0be');
+            stroke('#747d8c');
+            strokeWeight(1);
+            ellipse(i * 53, -this.scaledData(this.data[i].median), 7, 7);
+        }
+        pop();
+    }
+
+    drawTitle(){
+        fill('#747d8c');
+        textSize(this.fontSize);
+        textAlign(CENTER, BOTTOM);
+        text(this.title, this.chartWidth / 2, -this.chartHeight - 20);
     }
 }
--- Week 03 - Day 02 - Charts with class/sketch.js
+++ Week 03 - Day 02 - Charts with class/sketch.js
@@ -1,37 +1,31 @@
 
 //let data = [30, 100, 90, 20, 180, 94];
 //let dataLabels = ["Oranges", "Bananas", "Lemons", "Limes", "Apples", "Grapes"];
-let data = [
-    {name:"Oranges", total:63}, 
-    {name:"Bananas", total:33},
-    {name:"Lemons", total:21},
-    {name:"Limes", total:43}
+let finaldata = [
+    { name:"2012", total:55000, values:[13000, 10000, 24000, 9000], median:27500 },
+    { name:"2013", total:88000, values:[8000, 20000, 40000, 20000], median:44000 },
+    { name:"2014", total:89000, values:[26000, 44000, 10000, 9000], median:44500 },
+    { name:"2015", total:79000, values:[32000, 7000, 25000, 15000], median:39500 },
+    { name:"2016", total:96000, values:[6000, 50000, 30000, 10000], median:48000 },
+    { name:"2017", total:68000, values:[30000, 20000, 8000, 10000], median:34000 },
+    { name:"2018", total:47000, values:[7000, 10000, 20000, 10000], median:23500 }
 ];
 
 let chart01;
 let chart02;
 
 function setup() {
-    createCanvas(700,500);
-    chart01 = new BarChart(data);
-    chart01.chartWidth = 200;
+    createCanvas(700,1000);
+    chart01 = new BarChart(finaldata);
+    chart01.chartWidth = 400;
     chart01.chartHeigt = 200;
     chart01.posX = 100;
-    chart01.posY = 400;
-    
-    chart02 = new BarChart(data);
-    chart01.chartWidth = 200;
-    chart01.chartHeigt = 200;
-    chart01.posX = 400;
     chart01.posY = 400;
 }
 
 
 function draw() {
-    background(50);
+    background(250);
     chart01.updateValues();
-    chart02.updateValues();
     chart01.render();
-    chart02.render();
-
 }   
